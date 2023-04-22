79ef968a79f5bee70cc4c902c88f392ae8550ed1
enderta <endertanver@gmail.com>
2023-04-22 17:19:00 +0100

    first commit

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+
+const about = () => {
+    return (
+        <div>
+        <h1>About</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            accusantium, voluptate, quod voluptates, voluptas quidem nemo
+            necessitatibus quos autem quia quae. Quisquam, quod. Quos, quae
+            voluptates. Quisquam, quae. Quisquam.
+        </p>
+        </div>
+    );
+}
+
+export default about;
---
+++ pages/posts/[...id].js
@@ -0,0 +1,12 @@
+
+import { useRouter } from 'next/router'
+
+export default function Post() {
+    const router = useRouter()
+
+    return (
+        <>
+            <h1>{router.query.id}</h1>
+        </>
+    )
+}
