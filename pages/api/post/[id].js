bb1bfa9e7f375bf9a8b13718155c3109ba1867ff
enderta <endertanver@gmail.com>
2023-04-23 18:34:56 +0100

    blog

--- pages/api/post/[id].js
+++ pages/api/post/[id].js
@@ -1,37 +1,108 @@
-import path from "path";
 import jwt from "jsonwebtoken";
-import {check, validationResult} from "express-validator";
-import {connectDb, releaseDb} from '../../db';
-import { useRouter } from 'next/router';
+import {connectDb} from '../../db';
 
+const secret = 'your_secret_key_here';
 
-    export default async function handler(req, res) {
-        const {method} = req;
-        const {client} = await connectDb();
-        const id = req.query.id;
-
-
-        if (method === 'GET') {
-            try {
-                const {rows} = await client.query(
-                    "SELECT * FROM blog_posts WHERE id = $1",
-                    [id]
-                );
-                res.status(200).json({
-                    status: "success",
-                    message: `blog post found`,
-                    data: {
-                        rows
-                    }
-                });
-            } catch (err) {
-                console.error(err);
-                res.status(500).json({
-                    status: "error",
-                    message: "Error fetching blog posts"
-                });
-            } finally {
-                client.release();
+export default async function handler(req, res) {
+    const id = req.query.id;
+    const token = req.headers.authorization;
+    const {client} = await connectDb();
+    const {method} = req
+    const decoded = jwt.verify(token, secret);
+    if (method === "GET") {
+        try {
+            if (!token) {
+                return res.status(401).json({message: 'Unauthorized'});
             }
+
+            const {rows} = await client.query(
+                'SELECT * FROM blog_posts WHERE id = $1',
+                [id]
+            );
+
+            if (!rows || rows.length === 0) {
+                return res.status(404).json({message: 'Post not found'});
+            }
+
+            return res.status(200).json({
+                status: 'success',
+                message: 'Blog post found',
+                data: {
+                    rows,
+                },
+            });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({message: 'Internal server error'});
+        } finally {
+            client.release();
         }
     }
+    if (method === "PUT") {
+        try {
+            if (!token) {
+                return res.status(401).json({message: 'Unauthorized'});
+            }
+            if (decoded.user.role !== "admin") {
+                return res.status(403).json({message: "Forbidden"});
+            }
+
+            const {title, content, author, image_url} = req.body;
+            const {rows} = await client.query(
+                "UPDATE blog_posts SET title = $1, content = $2, author = $3, image_url = $4 WHERE id = $5 RETURNING *",
+                [title, content, author, image_url, id]
+            );
+            if (rows.length === 0) {
+                return res.status(404).json({
+                    status: "error",
+                    message: "Blog post not found",
+                });
+            }
+            res.status(200).json({
+                status: "success",
+                message: "Blog post updated",
+                data: {
+                    rows
+                }
+            });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error");
+        } finally {
+            client.release();
+        }
+
+    }
+    if (method === "DELETE") {
+        try {
+            if (!token) {
+                return res.status(401).json({message: 'Unauthorized'});
+            }
+            if (decoded.user.role !== "admin") {
+                return res.status(403).json({message: "Forbidden"});
+            }
+            const {rows} = await client.query(
+                "DELETE FROM blog_posts WHERE id = $1 RETURNING *",
+                [id]
+            );
+            if (rows.length === 0) {
+                return res.status(404).json({
+                    status: "error",
+                    message: "Blog post not found",
+                });
+            }
+            res.status(200).json({
+                status: "success",
+                message: "Blog post deleted",
+                data: rows[0],
+            });
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send("Server error");
+        } finally {
+            client.release();
+        }
+    }
+}
+
+
